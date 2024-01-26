01498829c78488511f7fd04c67f772d8992e71bb
amaya saudade <iwaizumi.yoshi@gmail.com>
2024-01-27 00:45:41 +0300

    modify functions to be private

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -5,13 +5,13 @@
 //  Created by Darina Kirilenko on 25.01.2024.
 //
 
-import UIKit                     
+import UIKit
 
 final class ViewController: UIViewController {
     
 
     @IBOutlet var nextButton: UIButton!
-    
+
     @IBOutlet var redLightView: UIView!
     @IBOutlet var yellowLightView: UIView!
     @IBOutlet var greenLightView: UIView!
@@ -24,18 +24,18 @@ final class ViewController: UIViewController {
         setUpLights()
     }
     
-    func resetLights() {
+    private func resetLights() {
         redLightView.alpha = 0.3
         yellowLightView.alpha = 0.3
         greenLightView.alpha = 0.3
     }
     
-    func setUpNextButton() {
+    private func setUpNextButton() {
         nextButton.layer.cornerRadius = 10
         nextButton.setTitle("START", for: .normal)
     }
         
-    func setUpLights() {
+    private func setUpLights() {
         redLightView.layer.cornerRadius = 75
         yellowLightView.layer.cornerRadius = 75
         greenLightView.layer.cornerRadius = 75
